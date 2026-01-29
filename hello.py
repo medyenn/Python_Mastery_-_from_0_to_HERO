1059d7196ebda1315cd80575d9793398c42aeb6b
Mohamed Ennih <mennih@c2r5p12.1337.ma>
2026-01-30 00:07:28 +0100

    intro to pthon basics

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
 # asking for name
 name = input("What's ur name ? ").strip().capitalize().title()
 print(f"Hello, {name} !", "\"test separator place\"", sep=" / ", end="-_-")
-print("\"test prompt place\"")
+print('"test prompt place"')
