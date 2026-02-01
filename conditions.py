14149b5cdc6061a135dbe51857bd860dd7326fcd
Mohamed Ennih <mennih@c2r6p6.1337.ma>
2026-02-01 14:49:58 +0100

    Python Basics

--- conditions.py
+++ conditions.py
@@ -4,15 +4,16 @@ def main():
         print("The number is EVEN!")
     else:
         print("The number ODD!")
+    revenues()
 
 
 def compariseon():
     x = int(input("Enter the first value : "))
     y = int(input("Enter the second value : "))
     if x > y:
-        print(f"The first the value {x} is greater than the second value {y} !")
+        print(f"{x} is greater than {y} !")
     elif x < y:
-        print(f"The first value {x} is less than the second value {y} !")
+        print(f"{x} is less than {y} !")
     else:
         print("The two values are equal")
 
@@ -41,10 +42,20 @@ def grader():
 
 
 def parity(n):
-    if n % 2 == 0:
-        return 1
-    else:
-        return 0
+    return n % 2 == 0
+
+
+def revenues():
+    job = input("What is your Job title: ").strip().title()
+    match job:
+        case "Data Analyst" | "IT Project Manager":
+            print("Average Revenue : $100k per year")
+        case "Data Engineer" | "DevOps Engineer" | "Software Developer":
+            print("Average Revenue : $140k per year")
+        case "Data Scientist" | "Ai Engineer" | "Enterprise Architect":
+            print("Average Revenue : $220k per year")
+        case _:
+            print("I have no idea (>^o^<)")
 
 
 main()
